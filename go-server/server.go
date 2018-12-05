5178f432ee94d72c49f8967832a0dccde48d9400
Constantin Diaconescu <diaconescu.constantin@gmail.com>
2018-12-05 12:57:49 +0200

    Update server.go

--- go-server/server.go
+++ go-server/server.go
@@ -9,7 +9,15 @@ func handlerIndex(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("static/")))
+	changeHeaderThenServe := func(h http.Handler) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			// Set some header.
+			w.Header().Add("Keep-Alive", "300")
+			// Serve with the actual handler.
+			h.ServeHTTP(w, r)
+		}
+	}
+	http.Handle("/", changeHeaderThenServe(http.FileServer(http.Dir("static/"))))
 	http.HandleFunc("/about/", handlerIndex)
 
 	http.ListenAndServe(":3000", nil)
