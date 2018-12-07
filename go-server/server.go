08691bc401092a1df8d63b62c5e528b0c8621922
Constantin Diaconescu <diaconescu.constantin@gmail.com>
2018-12-07 08:39:12 +0200

    Update server.go

--- go-server/server.go
+++ go-server/server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
@@ -9,6 +14,8 @@ func handlerIndex(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	log.Println("Starting server...")
+
 	changeHeaderThenServe := func(h http.Handler) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			// Set some header.
@@ -20,5 +27,18 @@ func main() {
 	http.Handle("/", changeHeaderThenServe(http.FileServer(http.Dir("static/"))))
 	http.HandleFunc("/about/", handlerIndex)
 
-	http.ListenAndServe(":3000", nil)
+	log.Println("Listening on port 3000...")
+
+	s := http.Server{Addr: ":3000"}
+	go func() {
+		log.Fatal(s.ListenAndServe())
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
+	log.Println("Shutdown signal received, exiting...")
+
+	s.Shutdown(context.Background())
 }
